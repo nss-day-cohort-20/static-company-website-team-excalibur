760deba2c836f8caf6feb780b9fb09cb528e8f05
Greta Moseley <greta.moseley@gmail.com>
2017-05-28 11:37:03 -0500

    js page updated with print function, object creation function, object creation calls, card array containing objects, and function to iterate through the array.  Need to work on innerHTML next.

--- temporalJavaScripts.js
+++ temporalJavaScripts.js
@@ -8,10 +8,22 @@ var moonurl="images/moonbuzz.jpg"
 var caesarurl="images/caesardeath.jpg"
 var sparturl="images/spartanwarriors.jpg"
 //make sure to move images that we choose to the 
-//images folder and change these variables as needed
+//images folder and change these variables representing urls as needed
+
+//need print function to insert info into html at the end
+function printHTMLToDiv(message) {
+	var tourdiv= document.getElementById("basictour1");
+	//tourdiv holds a node, or a reference to the basictour1 div on 
+	//the html page (pointing to where we want to add text)
+
+	tourdiv.innerHTML = message; 
+	//will take on string passed in through message when the function
+	//is called below.
+}
+
 
 function makeSummaryObject(name, description, price, url){
-	var summary = {}
+	var summary = {};
 	summary.name = name;
 	summary.description = description;
 	summary.price = price;
@@ -20,39 +32,43 @@ function makeSummaryObject(name, description, price, url){
 	return summary;
 }
 
-//function with code to create objects 
+// above function with code to create an object- 
+//need to assign value to 
+//perameters next below and create each object
 
-var oregon = ("Oregon Trail Tour", "description", 1000, oregonurl);
-var pompeii=("Witness the Destruction of Pompeii", "description", 2000, pompurl);
-var firstflight=("First in Flight Tour", "description", 3000, firsturl);
-var triassic=("Triassic Park", "description", 4000, triurl);
-var olympics=("Ancient Greek Olympics Tour" );
-var moon=("The Lunar Landing");
-var caesar =("The Assassination of Julius Caesar");
-var spartans = ("The Battle of Thermopylae");
-
-function printHTML(message) {
-	var tourdiv= document.getElementById("basictour1");
-	
-}
+var oregon = makeSummaryObject("Oregon Trail Tour", "description", 1000, oregonurl);
+var pompeii = makeSummaryObject("Witness the Destruction of Pompeii", "description", 2000, pompurl);
+var firstflight = makeSummaryObject("First in Flight Tour", "description", 3000, firsturl);
+var triassic = makeSummaryObject("Triassic Park", "description", 4000, triurl);
+var olympics = makeSummaryObject("Ancient Greek Olympics Tour", "description", 5000, olympurl );
+var moon = makeSummaryObject("The Lunar Landing", "description", 6000, moonurl);
+var caesar = makeSummaryObject("The Assassination of Julius Caesar", "description", 7000, caesarurl);
+var spartans = makeSummaryObject("The Battle of Thermopylae", "description", 8000, sparturl);
 
 //pass information into each object (add desc, price, url)
+//call function to create each object and store value in a variable
+//that then becomes part of the array below
 
 var cardArray=[oregon, pompeii, firstflight, triassic, olympics, moon, caesar, spartans ];
 //array to hold objects that contain name, price, etc info above
 
+
 function iterateThroughArray(arr) {
 for (var i=0; i<cardArray.length; i++) {
 
-	makeSummaryObject(arr[i]); //call function to create the objects 
+console.log(cardArray[i]); //testing purposes only to see if the above worked
 
 	
+	}
 }
 
 //loop to run through each
 // object in the array and print to the DOM
 
+
 iterateThroughArray(cardArray);
+
+
 
 
 
